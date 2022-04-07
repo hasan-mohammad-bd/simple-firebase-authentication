873f7065d69e0098b861c512da0569f13505d3d6
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-07 09:40:39 +0600

    updating note

--- src/note.js
+++ src/note.js
@@ -3,39 +3,43 @@ firebase project steps:
 //=========================================
 steps:
 
-1. create a project (with meaning full name).
-2. fulfill the requirement.
-3. go to does right corner of the page > build > web > get started > install the firebase js SDK
-> install sdk > npm install firebase > 
-4. register to the firebase > and continue > 
-5. come to project setting > go to general setting > copy the firebase initialization >
-6.  create a file in src > then copy the firebase initialization > export default app > import {getAuth} from "firebase/auth"(to app.js) >
-7.  const auth  = getAuth(app) (in App.js) > console.firebase >
-8. authentication(below setting) > setStated > go to google sign in > enable this name and save >
-9. import google auth provider and take it in a variable>
 
-10. add event handler. here pass the code :
-signWithPopup(auth, provider).then(result => {
+1. create a project
+
+2. create a web project
+
+3. run npm install firebase
+
+4. copy the init and save it in a folder
+
+5. go to authentication 
+
+6. enable github or google
+
+7. fulfill the requirement and save
+
+8. go to does > build > web >get started> and copy the line of code to App.js : const auth = getAuth(app);(also import)(above the function component)
+
+9. go to does > build > web > google/github > copy the code: provider = googleAuthProvider() (also import)
+
+10. create a button for event handler. use googleSigninPopup function :
+  signInWithPopup(auth, provider)
+  .then((result) => {
     const user = result.user;
+    setUser(user);
     console.log(user);
-})
-.catch(error => {
-    condole.error("error", error)
-})
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
-11.  to show the user info to ui, need to use state
+11. create a button for google/github sign in. place the event handler
 
-//to use github authentication
-=================================
-1. go to authentication > sign in method > go to github
+12.  to show to UI use useState
 
-2. go to your personal github account > setting > register a new OAuth application > get authorized classback url from firebase github > copy & paste > "register application"
+13. show to ui ex: name is  user.displayName
 
-3. get the client id  and client secret from github and paste them to authentication and save.
 
-4. go does > authentication > web > github
-
-5. copy provider 
 
 6. set the event handler.
 
